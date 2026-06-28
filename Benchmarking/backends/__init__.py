f3b89d5eb6b1115d149305f50195b41d72563cae
Magomed <zhabiev@cip.ifi.lmu.de>
2026-06-28 10:36:52 +0200

    Add shapr backend

--- Benchmarking/backends/__init__.py
+++ Benchmarking/backends/__init__.py
@@ -1,13 +1,1 @@
-from .shap_backend import ShapTrueValueBackend, ShapApproxBackend
-from .shapiq_backend import ShapIQTrueValueBackend, ShapIQApproxBackend
-from .lightshap_backend import LightShapApproxBackend
-from .dalex_backend import DalexApproxBackend
-
-__all__ = [
-    "ShapTrueValueBackend",
-    "ShapApproxBackend",
-    "ShapIQTrueValueBackend",
-    "ShapIQApproxBackend",
-    "LightShapApproxBackend",
-    "DalexApproxBackend",
-]
+/Users/maga/PR_ModeAgnostic/PR_ModeAgnostic/Benchmarking/backends/__init__.py
